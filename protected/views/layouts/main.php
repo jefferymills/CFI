7074d6b81a19c06c6fb56a4dd9aa4948cfeaa71f
Jeff Mills <jeff@jefferymills.com>
2012-06-02 03:05:35 -0600

    Added less and javascript files and began marking up and styling

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -11,6 +11,16 @@
 
   <meta name="viewport" content="width=device-width">
 
+  <?php 
+	  $cs = Yii::app()->getClientScript();
+	  //javascript
+	  $cs->registerScriptFile(Yii::app()->baseUrl .'/js/libs/modernizr-2.5.3.js');
+	  $cs->registerScriptFile(Yii::app()->baseUrl .'/js/libs/paper.js');
+	  $cs->registerScriptFile('https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+	  //css
+	  $cs->registerCssFile(Yii::app()->baseUrl .'/css/style.css'); 
+  ?>
+
 </head>
 
 <body>
@@ -20,9 +30,41 @@
 
 	<header>
 
+		<div class="top-bar"></div>
+
+		<div class="logo">
+
+			<h1><?php echo CHtml::encode($this->pageTitle); ?></h1>
+
+		</div>
+
+		<div class="map-title">
+			
+			<h2>Stadium Capacity</h2>
+
+			<p class="description">As of 2012</p>
+
+		</div>
+
 	</header>
 
 	<div role="main">
+
+		<nav>
+
+			<button type="button" class="nav-button"></button>
+			
+			<ul>
+				<li>National Championships</li>
+				<li>Top 10 Finishes</li>
+				<li>Total Winning Percentage</li>
+				<li>BCS Bowl Wins</li>
+				<li>Annual Revenue</li>
+				<li class="current">Stadium Capacity</li>
+				<li>Top 25 Finishes</li>
+			</ul>
+
+		</nav>
 
 		<?php echo $content; ?>
 
